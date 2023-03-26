c53661944365228e4bfb43eaa95dd7267039990e
Nyaromollo <omolloenter@gmail.com>
2023-03-26 12:39:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 31
+function getSumAmountOfbatterybatches(batterybatches){
+let totalBatteries = 0;
+
+for (const batteryBatch of batteryBatches){
+    totalBatteries += batteryBatch. batch;
+};
+return totalBatteries;
+}
+const doubledAndSummed = [4,5,3,4,4,6,5].reduce(function(accumulator, element){ return element *2 + accumulator})
